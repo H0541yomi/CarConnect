898d094d18001b9d5bc0268667fc1f00c1c447d3
H0541yomi <ejcabalona727@gmail.com>
2025-08-15 22:50:18 -0400

    Changed database name in env template to give api perms to make requests to db

--- app/src/pages/PostsScroll.py
+++ app/src/pages/PostsScroll.py
@@ -1,2 +1,20 @@
+import requests
+import streamlit as st
+
+API_URL = "http://web-api:4000/posts"
+
+def fetch_posts():
+    response = requests.get(API_URL)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        st.write("Error fetching posts")
+        st.error("Error fetching posts")
+        return []
+
+posts = fetch_posts()
+st.write(posts)
+
+
 # Shows a bunch of post previews, showing their title and a small snippet of their body
 # Clicking on a post navigates to DetailedPost.py, using the post id
